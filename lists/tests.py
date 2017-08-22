e4fc9e8390a8e243e3a997b10f8330b661c16b02
heron <heron.rossi@hotmail.com>
2017-08-22 19:48:41 -0300

    First unit test. Dummy view

---
+++ lists/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+# Create your tests here.
+
+
+class HomePageTest(TestCase):
+    def test_resolve_root_url(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
