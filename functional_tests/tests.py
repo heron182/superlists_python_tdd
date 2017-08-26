96d232d1d405d4b52c668389e55f78e0fba77fc1
heron <heron.rossi@hotmail.com>
2017-08-26 11:19:18 -0300

    New FT for multiple lists added

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -58,12 +58,47 @@ class NewVisitorTest(LiveServerTestCase):
         # The list is once again updated with the new item
         self.wait_for_row_in_table('1 - Buy cigarrets')
         self.wait_for_row_in_table('2 - Search about French Literature')
-        self.fail('Finish the test')
 
-        # John notices that a unique url has been generated for
-        # his to-do list and there´s a little text explaining about it
+        # John closes the website
+        self.browser.quit()
 
-        # He visists the url and checks the to-do list is there and saved
+    def test_can_create_multiple_lists_with_unique_urls(self):
+        # John visits the website and start creating a new list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy fruit')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_table('1 - Buy fruit')
+
+        # He notices his list has a unique url
+        john_list_url = self.browser.current_url
+        self.assertRegex(john_list_url, '/list/.+')
+
+        # He goes to sleep
+        self.browser.quit()
+
+        # Emmon comes along and creates a new list
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
+
+        # Theres no sign of John´s list on Emmon´s list
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('1 - Buy fruit', page_text)
+
+        # Emmon types a new item for  it´s list
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Do groceries')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_table('1 - Do groceries')
+
+        # Emmon gets his own unique url
+        emmon_list_url = self.browser.current_url
+        self.assertRegex(list_url, '/list/.+')
+        self.assertNotEqual(john_list_url, emmon_list_url)
+
+        # Theres no trace of John´s list on the page
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('1 - Buy fruit', page_text)
 
 
 if __name__ == '__main__':
