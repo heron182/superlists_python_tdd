58f67eb359f80e3c4a41cae78c9117edcda9f0a4
Heron <hrossi@hpe.com>
2017-09-04 16:32:36 -0300

    Fabfile and templates updated for deployment

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,6 +1,7 @@
 from fabric.contrib.files import sed, append, exists
 from fabric.api import env, local, run
 import random
+import string
 
 REPO_URL = 'https://github.com/heron182/superlists_python_tdd'
 
@@ -19,20 +20,43 @@ def _get_latest_source(source_folder):
         run('git clone %s %s' % (REPO_URL, source_folder))
 
 
+def _update_settings(source_folder, site_name):
+    settings_file = '%s/superlists/settings.py' % source_folder
+    sed(settings_file, 'DEBUG = True', 'DEBUG = False')
+    sed(settings_file, 'ALLOWED_HOSTS =.+$',
+        'ALLOWED_HOSTS = ["%s"]' % site_name)
+    secret_key_file = '%s/superlists/secret_key.py' % source_folder
+    if not exists(secret_key_file):
+        secret = ''.join(random.SystemRandom().choice(string.ascii_letters)
+                         for _ in range(50))
+        append(secret_key_file, 'SECRET_KEY = "%s"' % secret)
+    append(settings_file, '\nfrom .secret_key import SECRET_KEY')
+
+
 def _update_virtualenv(site_folder):
-    virtualenv_folder = '%s/virtualenv' % source_folder
+    virtualenv_folder = '%s/virtualenv' % site_folder
     if not exists(virtualenv_folder + '/bin/pip'):
-        run('cd %s && python3.6 -m venv virtualenv')
-    run('cd %s/bin/pip install -r %s/source/requirements.txt' % (virtualenv_folder,
+        run('cd %s && python3.6 -m venv virtualenv' % site_folder)
+    run('%s/bin/pip install -r %s/source/requirements.txt' % (virtualenv_folder,
                                                                  site_folder))
 
 
-def deploy():
-    site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+def _update_static_files(source_folder):
+    run('cd %s && ../virtualenv/bin/python3.6 '
+        'manage.py collectstatic --noinput' % source_folder)
+
+
+def _update_database(source_folder):
+    run('cd %s && ../virtualenv/bin/python3.6 '
+        'manage.py migrate --noinput' % source_folder)
+
+
+def deploy(sitename):
+    site_folder = '/home/%s/sites/%s' % (env.user, sitename)
     source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
-    _update_settings(source_folder, env.host)
-    _update_virtualenv(source_folder)
+    _update_settings(source_folder, sitename)
+    _update_virtualenv(site_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
--- deploy_tools/provisioning_notes.md
+++ deploy_tools/provisioning_notes.md
@@ -18,11 +18,17 @@ eg, on Ubuntu:
 
 * see nginx.template.conf
 * replace SITENAME/USERNAME with, e.g., staging.my-domain.com/johndoe
+`sed -e "s/SITENAME/staging.my-domain.com/g" -e "s/USERNAME/johndoe/g" \
+    source/deploy_tools/nginx.tpl.conf | \
+    sudo tee /etc/nginx/sites-available/staging.my-domain.com`
 
 ## Systemd service
 
 * see gunicorn-systemd.template.service
 * replace SITENAME/USERNAME with, e.g., staging.my-domain.com/johndoe
+`sed -e "s/SITENAME/staging.my-domain.com/g" -e "s/USERNAME/johndoe/g" \
+source/deploy_tools/gunicorn-systemd.tpl.service | \
+sudo tee /etc/systemd/system/gunicorn-systemd.service`
 
 ## Folder structure:
 Assume we have a user account at /home/username
