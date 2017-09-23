5e59d310b7832fdabce4c079c5a320ba00cf5fd4
heron <heron.rossi@hotmail.com>
2017-09-23 10:40:34 -0300

    Working on login functionality

--- superlists/settings.py
+++ superlists/settings.py
@@ -114,6 +114,22 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
